a983113e0d581a09409848e98dc8690f487246d6
Bernardo Bas <bbas@siu.edu.ar>
2021-03-19 08:55:30 -0300

    Add charging animation

--- sync.sh
+++ sync.sh
@@ -15,10 +15,10 @@ source build/envsetup.sh
 # repopick -t android-11.0.0_r32
 
 cd frameworks/base/
-# git remote add camfix https://github.com/LG-G6/android_frameworks_base.git
-git fetch camfix
-# CAM FIXES & MICROG
-git cherry-pick ae63d669a09fd6aa329c52f12915b34a8d372d68 382163573c199308ebc8d3706e4b9405a3455d05 826b9344ccb9d533f763360203f54f42d542aa01
+# git remote add extras https://github.com/LG-G6/android_frameworks_base.git
+git fetch extras
+# CAM FIXES, MICROG, CHARGING ANIMATION
+git cherry-pick ae63d669a09fd6aa329c52f12915b34a8d372d68 382163573c199308ebc8d3706e4b9405a3455d05 826b9344ccb9d533f763360203f54f42d542aa01 57738d2b938c4beb456a596ebecf5f8db1f0521e
 cd ../../
 
 rm -rf packages/apps/Updater/
