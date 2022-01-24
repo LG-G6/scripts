197d91b307cad943ba80ba351efd8e6a8183be81
Bernardo Bas <bbas@siu.edu.ar>
2022-01-24 14:26:13 -0300

    Goodbye charging animation, hallo new kernel
    * Removed charging animation
    * Moved to v30b branch
    * Added new prebuilt tools

--- extras.sh
+++ extras.sh
@@ -8,11 +8,11 @@ git cherry-pick 826b9344ccb9d533f763360203f54f42d542aa01
 cd ../../
 
 # Charging animation
-cd frameworks/base/
-git cherry-pick 57738d2b938c4beb456a596ebecf5f8db1f0521e
-cd ../../
+# cd frameworks/base/
+# git cherry-pick 57738d2b938c4beb456a596ebecf5f8db1f0521e
+# cd ../../
 
 # PixelProps
 cd frameworks/base/
-git cherry-pick 6b231cc5183de52c4c5573dc0266e8a0bcfb27e4 238e6d984bd26f09205c08161582931a0eeb9a2c
+git cherry-pick 6b231cc5183de52c4c5573dc0266e8a0bcfb27e4 238e6d984bd26f09205c08161582931a0eeb9a2c 8f590383144876e99450bf3f11c195c5ae0db755
 cd ../../
