9650b2cdb39cec7931410dba913a7dfa35241b6e
Bernardo Bas <31733216+BernardoBas@users.noreply.github.com>
2022-01-26 12:35:21 -0300

    Update SF Carbon folders

--- upload.sh
+++ upload.sh
@@ -3,22 +3,22 @@
 cd ~/android/builds/carbon/us997/ && rm *.*
 cd ~/android/carbon9/out/target/product/us997/ && mv *.zip *.md5sum ~/android/builds/carbon/us997/
 cd ~/android/builds/carbon/us997/ && rm carbon_us997-ota*.zip *.md5sum
-sftp bernardobas@frs.sourceforge.net:/home/pfs/project/lg-g6/crDroid\ 7/US997/ <<< $'put *.*'
+sftp bernardobas@frs.sourceforge.net:/home/pfs/project/lg-g6/CarbonROM\ 9/US997/ <<< $'put *.*'
 
 cd ~/android/builds/carbon/h872/ && rm *.*
 cd ~/android/carbon9/out/target/product/h872/ && mv *.zip *.md5sum ~/android/builds/carbon/h872/
 cd ~/android/builds/carbon/h872/ && rm carbon_h872-ota*.zip *.md5sum
-sftp bernardobas@frs.sourceforge.net:/home/pfs/project/lg-g6/crDroid\ 7/H872/ <<< $'put *.*'
+sftp bernardobas@frs.sourceforge.net:/home/pfs/project/lg-g6/CarbonROM\ 9/H872/ <<< $'put *.*'
 
 cd ~/android/builds/carbon/h870ds/ && rm *.*
 cd ~/android/carbon9/out/target/product/h870ds/ && mv *.zip *.md5sum ~/android/builds/carbon/h870ds/
 cd ~/android/builds/carbon/h870ds/ && rm carbon_h870ds-ota*.zip *.md5sum
-sftp bernardobas@frs.sourceforge.net:/home/pfs/project/lg-g6/crDroid\ 7/H870DS/ <<< $'put *.*'
+sftp bernardobas@frs.sourceforge.net:/home/pfs/project/lg-g6/CarbonROM\ 9/H870DS/ <<< $'put *.*'
 
 cd ~/android/builds/carbon/h870/ && rm *.*
 cd ~/android/carbon9/out/target/product/h870/ && mv *.zip *.md5sum ~/android/builds/carbon/h870/
 cd ~/android/builds/carbon/h870/ && rm carbon_h870-ota*.zip *.md5sum
-sftp bernardobas@frs.sourceforge.net:/home/pfs/project/lg-g6/crDroid\ 7/H870/ <<< $'put *.*'
+sftp bernardobas@frs.sourceforge.net:/home/pfs/project/lg-g6/CarbonROM\ 9/H870/ <<< $'put *.*'
 
 echo -e "\nAndroid builds completed and uploaded\n";
 
