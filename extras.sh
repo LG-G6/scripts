25eff313b6187ab00beb23183def284a3b5fd9f3
Bernardo Bas <bernardobas@gmail.com>
2022-03-23 11:09:31 -0300

    Update picks and reset script

--- extras.sh
+++ extras.sh
@@ -4,15 +4,15 @@
 
 # microG support
 cd frameworks/base/
-git cherry-pick 826b9344ccb9d533f763360203f54f42d542aa01
+git cherry-pick f93240a9fe3e74e252e6e69db6993bee751a3515
 cd ../../
 
 # Charging animation
-# cd frameworks/base/
-# git cherry-pick 57738d2b938c4beb456a596ebecf5f8db1f0521e
-# cd ../../
+cd frameworks/base/
+git cherry-pick 16be597d1893c3865a910d8926ab2a42424534c0
+cd ../../
 
 # PixelProps
 cd frameworks/base/
-git cherry-pick 6b231cc5183de52c4c5573dc0266e8a0bcfb27e4 238e6d984bd26f09205c08161582931a0eeb9a2c 8f590383144876e99450bf3f11c195c5ae0db755 771e4a1224f25a6449f445cf066268dc2d9af5ed 1598620ba709246c9d5af5a556bd95388ecf1b80
+git cherry-pick bdf722ffadd92ed4ef8b89fcd9e3b7338da1217b
 cd ../../
--- reset-g6.sh
+++ reset-g6.sh
@@ -9,6 +9,8 @@ rm -rf frameworks/base/ && repo sync --force-sync frameworks/base/
 rm -rf vendor/lge/ && repo sync --force-sync vendor/lge/
 rm -rf kernel/lge/msm8996/ && repo sync --force-sync kernel/lge/msm8996/
 rm -rf device/lge/* && repo sync --force-sync device/lge/h870 && repo sync --force-sync device/lge/h870ds && repo sync --force-sync device/lge/h872 && repo sync --force-sync device/lge/us997 && repo sync --force-sync device/lge/msm8996-common && repo sync --force-sync device/lge/g6-common
+repo sync --force-sync LG-G6/gcc-10.3-aarch64
+repo sync --force-sync LG-G6/gcc-10.3-arm
 
 source build/envsetup.sh
 source scripts/fixes.sh
