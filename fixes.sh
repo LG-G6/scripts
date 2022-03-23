01f5366d0253e4a34c03d39c2a714970a44cbc7d
Bernardo Bas <31733216+BernardoBas@users.noreply.github.com>
2022-03-23 11:27:29 -0300

    Update camera fix picks

--- fixes.sh
+++ fixes.sh
@@ -3,7 +3,7 @@
 # Cam fix for LG G6
 cd frameworks/base/
 git fetch https://github.com/LG-G6/android_frameworks_base.git lineage-18.1
-git cherry-pick 2bb23e9ca3adb657b64147e29c53fd7dac383bb6 e4af95d7a4da898ef0ac2e9085806cd0f8cbc0b2
+git cherry-pick 08284b856f1fc64f8695daefcd0f4e75fdba1219 048c46b54c8fd7ea76db12abb5cbefde76f6b38c
 cd ../../
 
 # Fix for LG G6 TCP errors
