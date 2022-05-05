31c48c12a6e61ba4e5fe8d7c68f0a3cfc8efa6f4
Bernardo Bas <31733216+BernardoBas@users.noreply.github.com>
2022-05-05 15:08:40 -0300

    ASB: Remove picks already upstreamed

--- extras.sh
+++ extras.sh
@@ -133,12 +133,12 @@ cd frameworks/base/ && git fetch https://github.com/LineageOS/android_frameworks
 cd frameworks/base/ && git fetch https://github.com/LineageOS/android_frameworks_base refs/changes/23/328223/1 && git cherry-pick FETCH_HEAD && cd ../../
 cd frameworks/base/ && git fetch https://github.com/LineageOS/android_frameworks_base refs/changes/24/328224/1 && git cherry-pick FETCH_HEAD && cd ../../
 cd frameworks/base/ && git fetch https://github.com/LineageOS/android_frameworks_base refs/changes/25/328225/1 && git cherry-pick FETCH_HEAD && cd ../../
-cd packages/apps/Nfc && git fetch https://github.com/LineageOS/android_packages_apps_Nfc refs/changes/26/328226/1 && git cherry-pick FETCH_HEAD && cd ../../../
+# cd packages/apps/Nfc && git fetch https://github.com/LineageOS/android_packages_apps_Nfc refs/changes/26/328226/1 && git cherry-pick FETCH_HEAD && cd ../../../
 cd packages/apps/Settings && git fetch https://github.com/LineageOS/android_packages_apps_Settings refs/changes/27/328227/1 && git cherry-pick FETCH_HEAD && cd ../../../
 cd packages/providers/MediaProvider && git fetch https://github.com/LineageOS/android_packages_providers_MediaProvider refs/changes/28/328228/1 && git cherry-pick FETCH_HEAD && cd ../../../
-cd system/bt && git fetch https://github.com/LineageOS/android_system_bt refs/changes/29/328229/1 && git cherry-pick FETCH_HEAD && cd ../../
-#cd vendor/nxp/opensource/commonsys/packages/apps/Nfc && git fetch https://github.com/LineageOS/android_vendor_nxp_opensource_packages_apps_Nfc refs/changes/30/328230/1 && git cherry-pick FETCH_HEAD && cd ../../../../../../../
-cd vendor/qcom/opensource/commonsys/system/bt && git fetch https://github.com/LineageOS/android_vendor_qcom_opensource_system_bt refs/changes/31/328231/1 && git cherry-pick FETCH_HEAD && cd ../../../../../../
+# cd system/bt && git fetch https://github.com/LineageOS/android_system_bt refs/changes/29/328229/1 && git cherry-pick FETCH_HEAD && cd ../../
+# cd vendor/nxp/opensource/commonsys/packages/apps/Nfc && git fetch https://github.com/LineageOS/android_vendor_nxp_opensource_packages_apps_Nfc refs/changes/30/328230/1 && git cherry-pick FETCH_HEAD && cd ../../../../../../../
+# cd vendor/qcom/opensource/commonsys/system/bt && git fetch https://github.com/LineageOS/android_vendor_qcom_opensource_system_bt refs/changes/31/328231/1 && git cherry-pick FETCH_HEAD && cd ../../../../../../
 cd build/make/ && git fetch https://github.com/LineageOS/android_build refs/changes/44/328344/1 && git cherry-pick FETCH_HEAD && cd ../../
 
 printf "\n"
