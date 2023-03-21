53e6d53e0cec10a89027e17e90962475631654e4
Bernardo Bas <bbas@siu.edu.ar>
2023-03-21 09:38:11 -0300

    Bringup Lineage 19.1

--- build-g5.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-source scripts/sync-g5.sh
-make clean
-brunch h830
-brunch h850
-brunch rs988
-source scripts/upload-g5.sh
--- sync-g6.sh
+++ sync-g6.sh
@@ -5,11 +5,7 @@ rm -rf vendor/lge/
 rm -rf frameworks/base/
 rm -rf kernel/lge/msm8996/
 rm -rf device/lge/msm8996-common/
-rm -rf device/lge/g6-common/
-rm -rf device/lge/h870/
-rm -rf device/lge/h870ds/
-rm -rf device/lge/h872/
-rm -rf device/lge/us997/
+rm -rf device/lge/
 
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 source build/envsetup.sh
