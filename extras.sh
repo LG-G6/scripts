a0b49c9efa5451c10a0be861d83a13fce41ab1ef
Bernardo Bas <bbas@siu.edu.ar>
2023-03-23 15:01:20 -0300

    Add support for microG

--- extras.sh
+++ extras.sh
@@ -3,6 +3,15 @@
 # S_asb_2023-03
 # repopick -t S_asb_2023-03
 
+# microG support
+cd packages/modules/Permission/
+git fetch https://github.com/LG-G6/android_packages_modules_Permission.git lineage-19.1
+git cherry-pick 353836c8e3e13005c557a52a84d5edcbeb840346
+cd ../../../
+cd frameworks/base/
+git cherry-pick f2f1deb9929e08b7e5f560cc833a17a9ce645b87
+cd ../../
+
 # Charging animation
 cd frameworks/base/
 git cherry-pick 4378ebf9dfa5652398df60da3f48911babbd9307
