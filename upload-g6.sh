646a0e1555d71247bc49fdb67bab90473bcd0dca
Bernardo Bas <bbas@siu.edu.ar>
2023-04-18 09:40:47 -0300

    Rework G5 and G6 uploads

---
+++ upload-g6.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+cd ~/android/builds/lineage/us997/ && rm *.*
+cd ~/android/los19/out/target/product/us997/ && mv *.zip *.md5sum ~/android/builds/lineage/us997/
+cd ~/android/builds/lineage/us997/ && rm lineage_us997-ota*.zip *.md5sum
+sftp bernardobas@frs.sourceforge.net:/home/pfs/project/lucye/LineageOS\ 19.1/US997/ <<< $'put *.*'
+
+cd ~/android/builds/lineage/h872/ && rm *.*
+cd ~/android/los19/out/target/product/h872/ && mv *.zip *.md5sum ~/android/builds/lineage/h872/
+cd ~/android/builds/lineage/h872/ && rm lineage_h872-ota*.zip *.md5sum
+sftp bernardobas@frs.sourceforge.net:/home/pfs/project/lucye/LineageOS\ 19.1/H872/ <<< $'put *.*'
+
+cd ~/android/builds/lineage/h870/ && rm *.*
+cd ~/android/los19/out/target/product/h870/ && mv *.zip *.md5sum ~/android/builds/lineage/h870/
+cd ~/android/builds/lineage/h870/ && rm lineage_h870-ota*.zip *.md5sum
+sftp bernardobas@frs.sourceforge.net:/home/pfs/project/lucye/LineageOS\ 19.1/H870/ <<< $'put *.*'
+
+echo -e "\nAndroid builds completed and uploaded\n";
+
+cd ~/android/los19/
