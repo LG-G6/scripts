81f3e3ca0ef7e9f83ad47566ba40c498fb21b1fd
Bernardo Bas <31733216+BernardoBas@users.noreply.github.com>
2023-04-19 11:38:34 -0300

    Upload beta to Archive folder

--- upload-beta.sh
+++ upload-beta.sh
@@ -3,22 +3,22 @@
 cd ~/android/builds/lineage/us997/ && rm *.*
 cd ~/android/los18/out/target/product/us997/ && mv *.zip *.md5sum ~/android/builds/lineage/us997/
 cd ~/android/builds/lineage/us997/ && rm lineage_us997-ota*.zip *.md5sum
-sftp bernardobas@frs.sourceforge.net:/home/pfs/project/lg-g6/Beta/LineageOS\ 18.1/US997/ <<< $'put *.*'
+sftp bernardobas@frs.sourceforge.net:/home/pfs/project/lg-g6/Archive/Beta/LineageOS\ 18.1/US997/ <<< $'put *.*'
 
 cd ~/android/builds/lineage/h872/ && rm *.*
 cd ~/android/los18/out/target/product/h872/ && mv *.zip *.md5sum ~/android/builds/lineage/h872/
 cd ~/android/builds/lineage/h872/ && rm lineage_h872-ota*.zip *.md5sum
-sftp bernardobas@frs.sourceforge.net:/home/pfs/project/lg-g6/Beta/LineageOS\ 18.1/H872/ <<< $'put *.*'
+sftp bernardobas@frs.sourceforge.net:/home/pfs/project/lg-g6/Archive/Beta/LineageOS\ 18.1/H872/ <<< $'put *.*'
 
 cd ~/android/builds/lineage/h870ds/ && rm *.*
 cd ~/android/los18/out/target/product/h870ds/ && mv *.zip *.md5sum ~/android/builds/lineage/h870ds/
 cd ~/android/builds/lineage/h870ds/ && rm lineage_h870ds-ota*.zip *.md5sum
-sftp bernardobas@frs.sourceforge.net:/home/pfs/project/lg-g6/Beta/LineageOS\ 18.1/H870DS/ <<< $'put *.*'
+sftp bernardobas@frs.sourceforge.net:/home/pfs/project/lg-g6/Archive/Beta/LineageOS\ 18.1/H870DS/ <<< $'put *.*'
 
 cd ~/android/builds/lineage/h870/ && rm *.*
 cd ~/android/los18/out/target/product/h870/ && mv *.zip *.md5sum ~/android/builds/lineage/h870/
 cd ~/android/builds/lineage/h870/ && rm lineage_h870-ota*.zip *.md5sum
-sftp bernardobas@frs.sourceforge.net:/home/pfs/project/lg-g6/Beta/LineageOS\ 18.1/H870/ <<< $'put *.*'
+sftp bernardobas@frs.sourceforge.net:/home/pfs/project/lg-g6/Archive/Beta/LineageOS\ 18.1/H870/ <<< $'put *.*'
 
 echo -e "\nAndroid builds completed and uploaded\n";
 # notify-send Android "Builds uploaded" -i /usr/share/icons/Moka/96x96/apps/android-studio.png
