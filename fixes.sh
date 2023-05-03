b9f92914c773c91ca9b9fb9d2e3afd21c715e4a1
Bernardo Bas <bernardobas@gmail.com>
2023-05-02 23:06:02 -0300

    Switch back to Lineage repositories
    - Update roomservice
    - Apply mixer commit to g6-common

--- fixes.sh
+++ fixes.sh
@@ -11,3 +11,9 @@ cd packages/apps/Nfc
 git fetch https://github.com/LineageOS/android_packages_apps_Nfc.git lineage-18.1
 git cherry-pick 3a5991582a2abaddda11afa35d70f3e468ea1e3c e675f520f5d156463364e73458eb14b3a77ebc97
 cd ../../../
+
+# Mixer: adjust input volume levels 
+cd device/lge/g6-common
+git fetch https://github.com/LG-G6/android_device_lge_g6-common.git lineage-19.1
+git cherry-pick b3edeba5ac6500c145fec7222ffc696c9b819af0
+cd ../../../
