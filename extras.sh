e9fca89408543154041ae99d44e20a766e943052
Bernardo Bas <31733216+BernardoBas@users.noreply.github.com>
2023-06-13 15:19:05 -0300

    PixelPropsUtils: Update fingerprints to June 2023 release

--- extras.sh
+++ extras.sh
@@ -15,5 +15,5 @@ cd ../../
 
 # PixelProps
 cd frameworks/base/
-git cherry-pick a7bf62448e1631e77508bb2fb822d12a3d8fa3a1 43934d737d6f503c5f07350210a502f4e657a4f2
+git cherry-pick a7bf62448e1631e77508bb2fb822d12a3d8fa3a1 43934d737d6f503c5f07350210a502f4e657a4f2 f0f86537c2db8bb52b43f6466998c488d229e2dc
 cd ../../
