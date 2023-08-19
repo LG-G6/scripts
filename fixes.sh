59919113d8038dcaaf0dc654269d00f7e2830d53
Bernardo Bas <bernardobas@gmail.com>
2023-08-19 17:07:54 -0300

    Back to enforcing builds

--- fixes.sh
+++ fixes.sh
@@ -11,6 +11,3 @@ cd device/lge/g6-common
 git fetch https://github.com/LG-G6/android_device_lge_g6-common.git dev/lineage-19.1
 git cherry-pick b3edeba5ac6500c145fec7222ffc696c9b819af0
 cd ../../../
-
-# SELinux: let's keep it permissive
-repopick -f 350849
